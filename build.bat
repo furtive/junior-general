f8ffd8d375ec7a9723f540b87987af6d98c71981
Chris Lamothe <chrislamothe@gmail.com>
2022-05-10 18:25:20 -0600

    Trademark and branding fixes
    Removed any risky trademark wording.

--- build.bat
+++ build.bat
@@ -2,12 +2,12 @@
 
 if not exist dist mkdir dist
 call html-minifier --minify-css --collapse-whitespace index.html -o dist/index.html
-call uglifyjs erisk.js -m toplevel -c -o dist/erisk.js
+call uglifyjs jg.js -m toplevel -c -o dist/jg.js
 
-if exist dist\erisk.zip del dist\erisk.zip
-zip -Xjq9 dist\erisk.zip dist\index.html dist\erisk.js
+if exist dist\jg.zip del dist\jg.zip
+zip -Xjq9 dist\jg.zip dist\index.html dist\jg.js
 
-CALL :FileSize dist/erisk.zip FileSize
+CALL :FileSize dist/jg.zip FileSize
 echo Final size: %FileSize% bytes
 
 goto :EOF
